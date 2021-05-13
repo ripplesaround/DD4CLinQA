ab1767b99eb294b8e67b890389f926ba63abd3f8
Ripples <37956697+ripplesaround@users.noreply.github.com>
2021-05-13 10:16:50 +0800

    界面

---
+++ apply/interface.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+'''
+Author: ripples
+Email: ripplesaround@sina.com
+
+date: 2021/5/13 10:13
+desc:  用python写一个界面用于显示结果
+'''
+
+
+import tkinter
+
+
+if __name__ == "__main__":
+    top = tkinter.Tk()
+    # 进入消息循环
+    top.mainloop()
